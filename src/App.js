be2aca107eec7a725758cf56183a5a3a23c0f2e9
Miguel Lima <miguellimaoliver97@gmail.com>
2022-02-24 19:02:00 -0300

    Cards, Props, Renderizacao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Form from "./components/Form";
+import Card from './components/Card'
 
 
 class App extends React.Component {
   render() {
     return (
+      <>
       <Form />
+      <Card />
+      </>
     );
   }
 }
